bdd030228949ba7c67f59dfafe76a39956e6f55c
shpaginalexei <shpagin05@gmail.com>
2023-09-09 20:21:32 +0300

    Add struct Pipe and Station

--- Pipeline Transport System (PTS)/Pipeline Transport System (PTS).cpp
+++ Pipeline Transport System (PTS)/Pipeline Transport System (PTS).cpp
@@ -1,8 +1,25 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
+struct Pipe {
+    string name;
+    double lenght;
+    int diameter;
+    bool in_repair;
+};
+
+struct Station {
+    string name;
+    int num_workshops;
+    int workshops_in_operation;
+    double efficiency;
+};
+
 int main()
 {
-    cout << "Hello World!\n" << endl;
+    vector<Pipe> pipes = {};
+    vector<Station> stations = {};
+    return 0;
 }
